5ae258d184d840e5b2cc97728a494d64803a54fc
Carlos Queiroz <carlostgti@outlook.com>
2021-02-24 14:18:26 -0300

    configuring and add some test cases

---
+++ atomic-design/exemplos/src/tools/test-utils.tsx
@@ -0,0 +1,21 @@
+import { FC, ReactElement } from 'react'
+import { render, RenderOptions } from '@testing-library/react'
+import { ThemeProvider } from 'styled-components'
+import theme from '../styles/theme'
+
+const AllTheProviders: FC = ({ children }) => {
+  return (
+    <ThemeProvider theme={theme}>
+      {children}
+    </ThemeProvider>
+  )
+}
+
+const customRender = (
+  ui: ReactElement,
+  options?: Omit<RenderOptions, 'queries'>
+) => render(ui, { wrapper: AllTheProviders, ...options })
+
+export * from '@testing-library/react'
+
+export { customRender as render }
